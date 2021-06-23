8cd10776870ec3e96730f0a26a2b309cf1c5f377
theMahammad <hssjxie@gmail.com>
2021-06-23 15:20:43 +0400

    Üçüncü həftənin üçüncü tapşırığı edildi

---
+++ Tasks/Week__3/Task_3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(100);
+            int chancesForEstimate = 5;
+            Console.WriteLine("1-den 100-e qeder ededler arasında bir texmin edin :");
+            int estimateFromClient;
+            int usedChances = 0;
+            do
+            {
+                Console.Write($"{usedChances+1}.şans : ");
+                estimateFromClient = int.Parse(Console.ReadLine());
+                usedChances++;
+                if (usedChances == chancesForEstimate&&estimateFromClient!=randomNumber)
+                {
+                    Console.WriteLine($"Sizin texmin haqqınız bitmişdir :( \nDoğru cavab :{randomNumber}");
+                }
+                else if (estimateFromClient == randomNumber)
+                {
+                    Console.WriteLine("Tebrikler,doğru bildiniz!");
+                    break;
+                }else if (estimateFromClient > randomNumber)
+                {
+                    Console.WriteLine("Biraz daha aşağı düşün");
+                }
+                else
+                {
+                    Console.WriteLine("Biraz daha yuxarı çıxın");
+                }
+                
+            } while (usedChances<chancesForEstimate);
+        }
+    }
+}
