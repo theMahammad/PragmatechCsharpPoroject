3f4c82780568ed90af0e3114840b0629660a9d81
theMahammad <hssjxie@gmail.com>
2021-06-23 15:36:18 +0400

    Üçüncü həftənin- cü tapşırığı edildi

---
+++ Tasks/Week__3/Task_4(find_factorial)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_4_find_factorial_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { Console.Write("Faktorialının tapılmasını istediyiniz ededi daxil edin : ");
+            int number = int.Parse(Console.ReadLine());
+            if (number == 0)
+            {
+                Console.WriteLine("0 daxil edile bilmez ");
+            }
+            int factorialOfNumber = 1;
+            
+            while (number > 0)
+            {
+                factorialOfNumber *= number;
+                number--;
+            }
+            Console.Write($"Daxil edilmiş ededin faktorialı : {factorialOfNumber} ");
+        }
+    }
+}
