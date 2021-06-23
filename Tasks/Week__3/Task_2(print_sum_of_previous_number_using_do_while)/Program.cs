4fd1f1070d0ffbf57e7b92c5751f5efc0ee8d44e
theMahammad <hssjxie@gmail.com>
2021-06-23 14:52:14 +0400

    Üçüncü həftənin ikinci tapşırığı edildi

---
+++ Tasks/Week__3/Task_2(print_sum_of_previous_number_using_do_while)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_2_print_sum_of_previous_number_using_do_while_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberFromClient;
+            int sum = 0;
+            do
+            {
+                Console.Write("Ekrana eded daxil edin : ");
+                numberFromClient = int.Parse(Console.ReadLine());
+                
+                if (numberFromClient == 0)
+                {
+                    Console.WriteLine($"Bundan evvel daxil edilen ededlerin cemi : {sum}");
+                }
+                sum += numberFromClient;
+            } while (numberFromClient!=0);
+        }
+    }
+}
