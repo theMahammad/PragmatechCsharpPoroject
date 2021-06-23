e6b3d27cdb27c94ea5b5b9cff7e55a01c875ed97
theMahammad <hssjxie@gmail.com>
2021-06-23 14:44:05 +0400

    Üçüncü həftənin 1-ci tapşırığı edildi

---
+++ Tasks/Week__3/Task_1(Print_every_even_number_from_1_to_100_and_their_sum)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_1_Print_every_even_number_from_1_to_100_and_their_sum_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = 1;
+            int finishNumber = 100;
+            int sum = 0;
+            while (startNumber<=finishNumber) {
+                if (startNumber % 2 == 0)
+                {
+                    Console.WriteLine(startNumber);
+                    sum += startNumber;
+                }
+                startNumber++;
+                    
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
