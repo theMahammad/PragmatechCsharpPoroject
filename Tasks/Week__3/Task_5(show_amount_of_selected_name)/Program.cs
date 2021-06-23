2d1d5f604a52fb59364f5c829aed0346ef95c725
theMahammad <hssjxie@gmail.com>
2021-06-23 16:19:36 +0400

    Üçüncü həftənin beşinci tapşırığı edildi

---
+++ Tasks/Week__3/Task_5(show_amount_of_selected_name)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_5_show_amount_of_selected_name_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Neçe ad daxil edeceksiniz? : ");
+            int n = int.Parse(Console.ReadLine());
+            string[] names = new string[n];
+            int i = 0;
+            while ( i<n)
+            {
+                Console.Write($"{i+1}. adı daxil edin : ");
+                names[i] = Console.ReadLine();
+                i++;
+            }
+            Console.WriteLine("Hansı adı seçmek isteyirsiniz?");
+            string selectedName = Console.ReadLine();
+            int amountOfSelectedName=0;
+            foreach(string name in names)
+            {
+                if (name == selectedName)
+                {
+                    amountOfSelectedName += 1;
+                }
+            }
+            Console.WriteLine($"Seçdiyiniz ad : {selectedName} | Seçdiyiniz adın siyahıdakı sayı : {amountOfSelectedName}");
+
+        }
+    }
+}
