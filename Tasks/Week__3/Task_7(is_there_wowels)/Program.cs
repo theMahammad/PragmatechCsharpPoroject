0e489bb1a1dbbe60dc55d9fdc41c8257378efcec
theMahammad <hssjxie@gmail.com>
2021-06-26 10:00:54 +0400

    Üçüncü həftənin yeddinci tapşırığı edildi

---
+++ Tasks/Week__3/Task_7(is_there_wowels)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task_7_is_there_wowels_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] vowels =new char[] { 'a', 'e', 'u', 'i', 'o', 'ü', 'ı' };
+            Console.Write("Yazı daxil edin : ");
+            string textFromClient = Console.ReadLine();
+            int count = 0;
+            foreach(char character in textFromClient)
+            {
+                foreach(char vowel in vowels)
+                {
+                    if (Char.ToLower(character) == vowel)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine($"Sait sayı : {count}");
+        }
+    }
+}
