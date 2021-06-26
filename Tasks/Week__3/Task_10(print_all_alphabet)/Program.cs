9cb057a80d25850123152ee5b66061eeb7a97579
theMahammad <hssjxie@gmail.com>
2021-06-26 10:57:56 +0400

    Üçüncü həftənin onuncu tapşırığı edildi

---
+++ Tasks/Week__3/Task_10(print_all_alphabet)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task_10_print_all_alphabet_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for(char i = 'a'; i <= 'z'; i++)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
