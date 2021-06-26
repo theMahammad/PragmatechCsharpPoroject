0b1ad4a5d098297564ce371476f1dd180acd7105
theMahammad <hssjxie@gmail.com>
2021-06-26 11:02:06 +0400

    Üçüncü həftənin son tapşırığı edildi

---
+++ Tasks/Week__3/Task_12(find_primaries_in_specific_range)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_12_find_primaries_in_specific_range_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Minimum ededi daxil edin :");
+            int minValue = int.Parse(Console.ReadLine());
+            Console.WriteLine("Maksimum ededi daxil edin");
+            int maxValue = int.Parse(Console.ReadLine());
+            for (int number = minValue; number <= maxValue; number++)
+            {
+                int amountOfDividers = 0;
+                for (int divider = 2; divider < number; divider++)
+                {
+                    if (number % divider == 0)
+                    {
+                        amountOfDividers++;
+                    }
+                }
+                if (amountOfDividers == 0)
+                {
+                    Console.WriteLine($"{number} sade ededdir");
+                }
+            }
+        }
+    }
+}
