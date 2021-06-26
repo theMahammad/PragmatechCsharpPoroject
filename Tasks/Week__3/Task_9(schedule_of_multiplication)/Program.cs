687e4c2e6fc6ce39aab322484dbfad741192305a
theMahammad <hssjxie@gmail.com>
2021-06-26 10:44:34 +0400

    Üçüncü həftənin doqquzuncu tapşırığı edildi

---
+++ Tasks/Week__3/Task_9(schedule_of_multiplication)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_9_schedule_of_multiplication_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for(int vurulan = 1; vurulan <= 10; vurulan++)
+            {
+                Console.WriteLine($"{vurulan}\n****");
+                for(int vuran = 0; vuran <= 10; vuran++)
+                {
+                    Console.Write($" {vuran * vurulan} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
