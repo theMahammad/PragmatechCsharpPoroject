25405d59ab2a37df40c43d52c86906ed3605f204
theMahammad <hssjxie@gmail.com>
2021-06-26 10:58:42 +0400

    Üçüncü həftənin onbirinci tapşırığı edildi

---
+++ Tasks/Week__3/Task_11(find_primaries)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task_11_find_primaries_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for(int number = 2; number <= 100; number++)
+            {
+                int amountOfDividers = 0;
+                for(int divider = 2; divider < number; divider++)
+                {
+                    if (number % divider == 0)
+                    {
+                        amountOfDividers++;
+                    }
+                }
+                if (amountOfDividers == 0)
+                {
+                    Console.WriteLine($"{number} sade ededdir");
+                }
+            }
+        }
+    }
+}
