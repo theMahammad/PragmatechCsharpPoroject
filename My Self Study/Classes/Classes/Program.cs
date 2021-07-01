58c66d559a3bb86b76987229d27e3a7bb3115f49
theMahammad <hssjxie@gmail.com>
2021-07-01 13:12:48 +0400

    Siniflər müəyyən qədər araşdırıldı

--- My Self Study/Classes/Classes/Program.cs
+++ My Self Study/Classes/Classes/Program.cs
@@ -16,7 +16,11 @@ namespace Classes
             Console.WriteLine(user.name);//Mehemmed
             Console.WriteLine(user.age);//15
             user.celebrateBirthday();//Yaşı 1 vahid artdı
-            Console.WriteLine(user.age);
+            Console.WriteLine(user.age);//16
+            User user1 = new User();//Defolt konstruktor çağrıldı,parametrlərə defolt dəyərləri ötürüldü(initialize olundu)
+            Console.WriteLine(user1.age); //0
+            User user2 = new User("Reşad Dağlı",35); //Parametrli konstruktor çağrıldı və sinfin içindəki dəyişənlərə mənimsədiləcək dəyərlər verildi
+            Console.WriteLine(user2.name);
         }
         //Sinifin təyin olunması
         public class User
@@ -27,8 +31,24 @@ namespace Classes
             public int age;
             public void celebrateBirthday(){
                 age++;
-}                
+            }
+            //Sinfin daxilində defolt olaraq parametrsiz konstruktor olur.O konstruktorun içində də dəyişənlər öz defolt dəyərlərinin(Məs : int - 0,string-null və s.) ötürüldüyü əmrlər icra olunur
+            public User()
+            {   //Defolt olaraq aşağıdakı kimi dəyər ötürmələri baş verir
+                //this.name = null;
+                //this.age = 0;
+            }
+            //Parametrli konstruktorlar
+            public User(string name,int age)
+            {
+                this.name = name;
+                this.age = age;
+                //burda "this" açarsözü sinfin dəyişənləri ilə metoda ötürülən dəyişənləri ayırd edə bilmək üçün istifadə olunur
+                //Struktlardan fərqli olaraq burda bütün dəyişənləri initialize etməyə  ehtiyac yoxdur
+            }
+
 
         }
+        
     }
 }
