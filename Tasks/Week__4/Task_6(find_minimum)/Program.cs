d54deee747061361119c9dacda17db504662cd52
theMahammad <hssjxie@gmail.com>
2021-07-05 09:15:21 +0400

    Dördüncü həftənin son tapşırığı da edildi

---
+++ Tasks/Week__4/Task_6(find_minimum)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_6_find_minimum_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[6] { 5,7,9,13,2,4};
+            //Let's find the minimum number of the array
+            //First Way
+            int min = numbers[0];
+            foreach(int element in numbers)
+            {
+                if (element < min)
+                {
+                    min = element;
+                }
+            }
+            Console.WriteLine($"Minimum : {min}");
+            Console.WriteLine("**********");
+            //Second way
+            //We will sort our array with built-in "Sort" method
+            Array.Sort(numbers);
+           
+            int min_ = numbers[0];
+            
+            Console.WriteLine($"Minimum : {min_}");
+
+
+        }
+    }
+}
